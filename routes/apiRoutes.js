9bc7a6a611e4e4d24913d1274606b35821484639
Greg Riss <gregriss23@gmail.com>
2021-03-05 22:02:50 -0500

    npm install express, added pseudocode to routes files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,61 @@
+// * The following API routes should be created:
+//   * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+//   * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+//   * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+
+// Setting up Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Loading data
-// link routs to a series of "data" sources
+// link routes to a series of "data" sources
 var tableData = require("");
 
 // Routing
 
 module.exports = function(app) {
-    // API get requests
-    app.get("", function(req, res){
-        res.json(tableData);
+    // API get request
+
+    // GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+    app.get("/api/notes", function(req, res){
+        // read db.json file
+        res.json();
+        // return all saved notes as json
+        return res.json(getNotes);
     })
 
-    app.get("", function(req, res) {
-        res.json(waitlistData);
+    // API post request
+    // POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+
+    app.post("/api/notes", function(req, res) {
+        // receive new note to save on request body
+
+        // add note to db.json file
+
+        // return new note to client
+
+
     })
 
-    // API post requests
+    // DELETE `/api/notes/:id`
+    // Should receive a query parameter containing the id of a note to delete
+    // Find a way to give each note a unique `id` when it's saved.
+    // In order to delete a note, you'll need to: 
 
-    app.post("", function(req, res) {
+    // read all notes from the `db.json` file, 
+    // remove the note with the given `id` property, and the
+    // rewrite the notes to the `db.json` file
 
-    })
-
-    app.post("", function(req,res) {
-
-    })
 }
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,22 @@
-// including path packate to get file paths
+// * The following HTML routes should be created:
+//   * GET `/notes` - Should return the `notes.html` file.
+//   * GET `*` - Should return the `index.html` file
+
+// including path package to get file paths
 const path = require('path');
 
+// including filesystem to read files 
+const fs = require('fs');
 
 module.exports = function(app) {
     // HTML GET requests
 
-    app.get("", function(req, res) {
+    // GET `*` - Should return the `index.html` file
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"))
+    });
+
+    app.get("/notes", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"))
     });
 }
